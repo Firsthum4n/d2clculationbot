23ae229a39103939f5566c3b06dfa1b514111b93
Zan <zanfilipp259@gmail.com>
2024-12-31 11:54:36 +0300

    ver 0.0.1

--- d2calcbot/main/calc_bot/bot.py
+++ d2calcbot/main/calc_bot/bot.py
@@ -3,68 +3,86 @@ from main.db_update.teams import create_or_update_teams
 from main.models import *
 
 
-radiant_pick = {
-    'team':
-        'Liquid',
-    'heroes':
-        ['Timbersaw', 'Dragon Knight', 'Templar Assassin', 'Muerta', 'Enchantress']
-    }
-
-dire_pick = {
-    'team':
-        'Tundra',
-    'heroes':
-        ['Beastmaster', 'Storm Spirit', 'Lifestealer', 'Clockwerk', 'Shadow Demon']
-    }
-
 def encryption(radiant, dire):
+    radiant_all_pick = encryption_level_1(radiant)
+    dire_all_pick =encryption_level_1(dire)
+
+    print(radiant_all_pick)
+    print(dire_all_pick)
+
+
+def encryption_level_1(team_pick):
+    team_all_pick = {
+        'team': [
+            {
+                'name': [],
+                'stats': []
+            }
+        ],
+        'players': [
+            {
+                'name': [],'stats': []
+            },
+            {
+                'name': [], 'stats': []
+            },
+            {
+                'name': [], 'stats': []
+            },
+            {
+                'name': [], 'stats': []
+            },
+            {
+                'name': [], 'stats': []
+            },
+        ],
+        'heroes': [
+            {
+                'name': [], 'stats': []
+            },
+            {
+                'name': [], 'stats': []
+            },
+            {
+                'name': [], 'stats': []
+            },
+            {
+                'name': [], 'stats': []
+            },
+            {
+                'name': [], 'stats': []
+            },
+        ]
+    }
+
     all_heroes = Heroes.objects.all()
-    all_teams = Teams.objects.all()
-    all_players = Players.objects.all()
-    radiant_all_pick = {
-        'team':[
-            {
-                'name':[],
-                'stats':[]
-            }
-        ],
-        'players':[
-            {
-                'name':[],
-                'stats':[]
-            }
-        ],
-        'heroes':[
-            {
-                'name': [],
-                'stats': []
-            }
-        ]
-   }
+    all_teams = Teams.objects.all().prefetch_related('players')
 
-    dire_all_pick = {
-        'team':[
-            {
-                'name':[],
-                'stats':[]
-            }
-        ],
-        'players':[
-            {
-                'name':[],
-                'stats':[]
-            }
-        ],
-        'heroes':[
-            {
-                'name': [],
-                'stats': []
-            }
-        ]
-   }
+    for team in all_teams:
+        if team_pick['team'] == team.name:
+            team_players = team.get_players()
+            team_all_pick['team'][0]['name'] = team.name
+            team_all_pick['team'][0]['stats'] = [team.rating, team.wins, team.losses]
+            for i in range(5):
+                team_all_pick['players'][i]['name'] = [team_players[i].name if team_players else None]
+                team_all_pick['players'][i]['stats'] = [team_players[i].games_played, team_players[i].wins if team_players else None]
 
 
-    for value in radiant.values():
-        for i in all_teams:
-            if value == i.name:
-                print(value)
+    count = 0
+    for h in team_pick['heroes']:
+        for hero in all_heroes:
+            if h == hero.name:
+                team_all_pick['heroes'][count]['name'] = [hero.name]
+                team_all_pick['heroes'][count]['stats'] = [hero.pro_pick, hero.pro_win, hero.pro_lose, hero.base_health, hero.base_health_regen,
+                                                           hero.base_mana, hero.base_mana_regen, hero.base_armor, hero.base_mr,
+                                                           hero.base_attack_min, hero.base_attack_max, hero.base_str, hero.base_agi,
+                                                           hero.base_int, hero.str_gain, hero.agi_gain, hero.int_gain, hero.attack_range,
+                                                           hero.projectile_speed, hero.attack_rate, hero.base_attack_time, hero.attack_point,
+                                                           hero.move_speed, hero.day_vision, hero.night_vision]
+                if count < 4:
+                    count += 1
+
+    return team_all_pick
+
+
+
