62010cf6eeb3c0a76931327f838dfa007d2082b1
Zan <zanfilipp259@gmail.com>
2025-01-07 03:05:11 +0300

    ver 0.0.1

--- d2calcbot/test.py
+++ d2calcbot/test.py
@@ -1,8 +1,15 @@
 import requests
 import json
 
-players_request = requests.get(f"https://api.opendota.com/api/teams/{9498970}/players")
+players_request = requests.get(f"https://api.opendota.com/api/teams/{7732977}/players")
 players_data = players_request.json()
 
 with open('players.json', 'w+') as players_file:
     json.dump(players_data, players_file, indent=4)
+
+
+
+request = requests.get(f"https://api.opendota.com/api/proPlayers")
+requestd = request.json()
+with open('pl.json', 'w+') as file:
+    json.dump(requestd, file, indent=4)
