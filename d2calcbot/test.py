f73824ef9b1bd60aa2808ef6f5017be33e72c12e
Zan <zanfilipp259@gmail.com>
2025-01-20 10:44:02 +0300

    000

--- d2calcbot/test.py
+++ d2calcbot/test.py
@@ -108,3 +108,58 @@ all_teams = []
 #
 # with open('main/jsf/teams.json', 'w+') as teams_file:
 #     json.dump(teams_data, teams_file, indent=4)
+
+######################################################################
+####################################################################
+##################################################################
+# import requests
+# import json
+# import time
+# def match_up_h():
+#     with open('main/jsf/hero_data_stats.json', 'r') as f:
+#         hero = json.load(f)
+#
+#     request_count = 0
+#     start_time = time.time()
+#
+#     for i in hero:
+#         if request_count >= 30 and time.time() - start_time < 60:
+#             sleep_time = 60 - (time.time() - start_time)
+#             print(f"Слишком много запросов. Ожидание {sleep_time:.2f} секунд...")
+#             time.sleep(sleep_time)
+#             request_count = 0
+#             start_time = time.time()
+#
+#         try:
+#             match_up = requests.get(f"https://api.opendota.com/api/heroes/{i['id']}/matchups", timeout=10)
+#             match_up.raise_for_status() # Проверяем на HTTP ошибки
+#             match_up_req = match_up.json()
+#             with open(f'main/calc_bot/matchupjson/{i["id"]}.json', 'w+') as f:
+#                 json.dump(match_up_req, f, indent=4)
+#             request_count += 1
+#             print(f"Успешно получены данные для героя {i['id']}. Запросов в минуту: {request_count}")
+#
+#         except requests.exceptions.RequestException as e:
+#             print(f"Ошибка при запросе к OpenDota API для героя {i['id']}: {e}")
+#
+#         time.sleep(0.1)
+#
+# match_up_h()
+
+
+##########################################################################################################
+#
+# p = requests.get(f"https://api.opendota.com/api/schema")
+# p_req = p.json()
+#
+#
+# with open('p.json', 'w+') as f:
+#     json.dump(p_req , f, indent=4)
+
+#
+# teams_request = requests.get("https://api.opendota.com/api/teams")
+# teams_data = teams_request.json()
+#
+# with open('main/jsf/teams.json', 'w+') as teams_file:
+#     json.dump(teams_data, teams_file, indent=4)
+
