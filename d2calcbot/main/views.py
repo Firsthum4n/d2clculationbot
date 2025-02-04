45aecd7ac00627aa72ee9cd5cbb8151de3b53b32
Jan Philipp <zanfilipp259@gmail.com>
2025-02-04 14:09:12 +0300

    0.0.5

--- d2calcbot/main/views.py
+++ d2calcbot/main/views.py
@@ -32,14 +32,14 @@ valid_data = DotaDataset(v_data)
 
 batch_size = 1
 model = MainNetwork(10, 32, 2)
-# model.load_state_dict(torch.load('main/calc_bot/actual_models/dota_model_ver0_1.pth'))
+model.load_state_dict(torch.load('main/calc_bot/actual_models/dota_model_ver0_2.pth'))
 
 
 criterion = nn.BCELoss()
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3, weight_decay=1e-4)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=6, gamma=0.1)
 
-EPOCHS = 2
+EPOCHS = 8
 
 
 dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
@@ -59,11 +59,11 @@ for epoch in range(EPOCHS):
 
     scheduler.step()
 print("Обучение завершено.")
-torch.save(model.state_dict(), 'main/calc_bot/actual_models/dota_model_ver0_2.pth')
+torch.save(model.state_dict(), 'main/calc_bot/actual_models/dota_model_ver0_3.pth')
 
 
 
-model.load_state_dict(torch.load('main/calc_bot/actual_models/dota_model_ver0_2.pth'))
+model.load_state_dict(torch.load('main/calc_bot/actual_models/dota_model_ver0_3.pth'))
 
 radiant_0 = 0
 dire_1 = 0
